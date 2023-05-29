96a681782564b6298af4ae8982fee6ff9724760f
Nanthakumar <nanthakumar.ananthan@innoboon.com>
2023-05-29 18:44:40 +0530

    change

--- setup.py
+++ setup.py
@@ -3,24 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='nandhatest',
     version='1.0.0',
-    author='test',
+    author='nandha',
     description='Function which file upload and download the files to the specified bucket',
     author_email='nanthakumar.ananthan@innoboon.com',
     packages=find_packages(),
+    url='https://github.com/nantha6/test'
     install_requires=[
-        'google-cloud-storage',
-        'boto3',
-        'botocore',
-        'selenium',
-        'webdriver-manager',
-        'Scrapy',
-    ],
-    classifiers=[
-        'Environment :: Console',
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.10.6',
-    ],
+        'paddleocr'
+    ]
 )
