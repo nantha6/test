b42a2a943bb1804b046dd8f81107890322a2a211
Nanthakumar <nanthakumar.ananthan@innoboon.com>
2023-05-29 18:45:39 +0530

    change

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='Function which file upload and download the files to the specified bucket',
     author_email='nanthakumar.ananthan@innoboon.com',
     packages=find_packages(),
-    url='https://github.com/nantha6/test'
+    url='https://github.com/nantha6/test',
     install_requires=[
         'paddleocr'
     ]
